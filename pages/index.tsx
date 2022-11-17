09d76c4bfffdbfb06e383742912214171a048fa5
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2022-11-17 10:45:51 -0300

    agora faltra ajustar a tabela

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import Cliente from './Cliente'
+import Tabela from './users/Tabela'
 const IndexPage = () => (
 <Layout title="Home | Next.js + TypeScript Example">
     <div className='text-neutral-500'>hello </div>
@@ -9,7 +10,9 @@ const IndexPage = () => (
   
    <div className='bg-black border-r-emerald-500 text-red-700'> <Link href="/Cliente">Cliente</Link>
     </div>
-   
+    
+    <div className='bg-black border-r-emerald-500 text-red-700'> <Link href="/users/Tabela">Tabela</Link>
+    </div>
    
   </Layout>
   )
--- pages/users/Tabela.tsx
+++ pages/users/Tabela.tsx
@@ -4,5 +4,5 @@ import Cliente from "../Cliente"
 import React from "react"
 import Titulo from "../Titulo"
  export default function Tabela (){
-return <table><Titulo /> <tr></tr><tr></tr><tr>dados<td><Cliente /></td></tr><tr><td>campos</td><td>id</td><td>idade</td><td>nome</td></tr></table>
+return  <table className= " FLEX justify-center items-center W w-full rounded-xl overflow-hidden bg bg-gradient-to-r from to-amber-400 text-amber-500 text-left P-4 bg-black "><Titulo /> <tr></tr><tr></tr><tr>dados<td><Cliente /></td></tr><tr><td>campos</td><td>id</td><td>idade</td><td>nome</td></tr></table>
  }
