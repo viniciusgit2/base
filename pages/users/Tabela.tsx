6ef53742597bd303b3fdf08c832a0d5fbf2559ad
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2022-11-17 10:20:14 -0300

    agora com uma quase tabela

---
+++ pages/users/Tabela.tsx
@@ -0,0 +1,8 @@
+
+import Cliente from "../Cliente"
+
+import React from "react"
+import Titulo from "../Titulo"
+ export default function Tabela (){
+return <table><Titulo /> <tr></tr><tr></tr><tr>dados<td><Cliente /></td></tr><tr><td>campos</td><td>id</td><td>idade</td><td>nome</td></tr></table>
+ }
