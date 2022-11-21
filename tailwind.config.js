e11a6a5676799db215277d5198ff53d34a184a92
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2022-11-21 08:52:26 -0300

    acho que pronto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge:{
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist:{
+
+  }
+,
   theme: {
     extend: {},
   },
   plugins: [],
+}
 }
