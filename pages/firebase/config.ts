155b8c6aceb30f80d0a70e0cb62b85a72eb49b58
viniciusgit2 <4xv6patelfin@gmail.com>
2023-03-29 11:44:28 -0300

    do 0

--- pages/firebase/config.ts
+++ pages/firebase/config.ts
@@ -1,5 +1,6 @@
+//arumar
 import firebase from "@firebase/app-compat";
-import firestore from "firebase/compat";
+
 if (!firebase.apps.length){
 apikey:process.env. NEXT_PUBLIC_apiKey
     authDomain:process.env.NEXT_PUBLIC_api_KeyauthDomain
