a53f572cf6ce3856fe56ee99b7e04252efc6aa7a
viniciusgit2 <4xv6patelfin@gmail.com>
2023-04-04 10:25:16 -0300

    muito lonnge

--- pages/src/js-tdd/music/principal/main.js
+++ pages/src/js-tdd/music/principal/main.js
@@ -2,6 +2,17 @@ import { expect, should } from "chai";
 
 describe(`music`,()=>{
 describe(`smoke test`,()=>{
-it (`should`)    
-})
-})
+it (`should exist search method`,()=>{
+    expect (search).to.exist
+
+}
+)
+}
+)
+}
+)
+
+
+
+
+
